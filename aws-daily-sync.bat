9e8ac2b39e9c36084f7fd82db05a0197d31c6ff7
Jmac <jonathan.macdonald@theinformationlab.co.uk>
2018-03-08 19:06:57 +0000

    initial add

--- aws-daily-sync.bat
+++ aws-daily-sync.bat
@@ -2,6 +2,6 @@
 
 echo %DATE% %TIME% Starting AWS S3 Backup sync >> aws-s3sync.log
 
-aws s3 sync d:\Backups s3://eif-tableau >> aws-s3sync.log
+aws s3 sync d:\Backups s3://tableau >> aws-s3sync.log
  
 echo %DATE% %TIME% AWS S3 Backup sync complete >> aws-s3sync.log
